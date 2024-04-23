eb2f13fb4549ebb55031bd87f6bf29a5124b69d9
Shishir393 <SHishir.karki393@gmail.com>
2024-04-23 17:46:39 -0500

    Add script.js file

---
+++ script.js
@@ -0,0 +1,9 @@
+alert('Hello, GitHub!');
+
+const h1 = document.querySelector('h1');
+h1.style.color = 'red';
+
+const liItems = document.querySelectorAll('li');
+liItems.forEach((item) => {
+    item.style.backgroundColor = 'lightblue';
+});
