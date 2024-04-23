0ce87f4bffb560a72a966e5245aa22c7739457ef
Shishir393 <98278277+Shishir393@users.noreply.github.com>
2024-04-23 18:06:34 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-alert('Hello, GitHub!');
-
-const h1 = document.querySelector('h1');
-h1.style.color = 'red';
-
-const liItems = document.querySelectorAll('li');
-liItems.forEach((item) => {
-    item.style.backgroundColor = 'lightblue';
-});
